7672630133750a321a5155ac24d1a52fc5de49f2
mralbinomonkey <127145426+mralbinomonkey@users.noreply.github.com>
2023-10-23 19:27:57 -0600

    Update home.js

--- static/script/home.js
+++ static/script/home.js
@@ -1,71 +1,15 @@
-/*
-  _____                   _                _     _                                                                      
- |  __ \                 | |              | |   | |                                                                     
- | |__) |   ___    _ __  | |_    ___    __| |   | |__    _   _                                                          
- |  ___/   / _ \  | '__| | __|  / _ \  / _` |   | '_ \  | | | |                                                         
- | |      | (_) | | |    | |_  |  __/ | (_| |   | |_) | | |_| |                                                         
- |_|       \___/  |_|     \__|  \___|  \__,_|   |_.__/   \__, |                                                         
-                                                          __/ |                                                         
-                                                         |___/                                                          
-                                _     _                     _       _   _          _                               _    
-     /\                        | |   | |                   | |     | \ | |        | |                             | |   
-    /  \     _ __ ___     ___  | |_  | |__    _   _   ___  | |_    |  \| |   ___  | |_  __      __   ___    _ __  | | __
-   / /\ \   | '_ ` _ \   / _ \ | __| | '_ \  | | | | / __| | __|   | . ` |  / _ \ | __| \ \ /\ / /  / _ \  | '__| | |/ /
-  / ____ \  | | | | | | |  __/ | |_  | | | | | |_| | \__ \ | |_    | |\  | |  __/ | |_   \ V  V /  | (_) | | |    |   < 
- /_/    \_\ |_| |_| |_|  \___|  \__| |_| |_|  \__, | |___/  \__|   |_| \_|  \___|  \__|   \_/\_/    \___/  |_|    |_|\_\
-                                               __/ |                                                                    
-                                              |___/                                                                     
-*/
 const tips = [
-    'Modify Inc&#173;ogni&#173;tos appearance & browser tab in <a href="#settings">settings.</a>',
-    'You can enable about:blank tab cloaking in <a href="#settings">settings.</a>',
-    'Access popular media & sites easily in <a href="#apps">apps.</a>',
-    'This <a href="https://github.com/amethystnetwork-dev/Incognito">unofficial In&#173;cog&#173;nito version</a> is made by Am&#173;et&#173;hy&#173;st Net&#173;wo&#173;rk.',
-    'Join the <a href="#community">Am&#173;et&#173;hyst Ne&#173;tw&#173;ork d&#173;i&#173;sco&#173;rd</a>',
-    'Get answers to questions in <a href="#support">support</a>',
-    `Check out <a onclick="(${ah.toString()})()">Ali&#173;enHu&#173;b</a>`
+    'the_plush on discord',
+    'very bored',
+    'updated today!',
+    'rofl',
+    'japanese url is crazy'
 ];
 
-// You can add more search engines if you want
-const searchProviders = {
-    google: {
-        mapQuery: (query) => `http://google.com/complete/search?q=${query}&client=${(["Chrome", "Firefox", "Safari"].filter(c => navigator.userAgent.includes(c))[0] || "Chrome").toLowerCase()}`,
-        parseResponse: (res) => JSON.parse(res)[1]
-    },
-    ddg: {
-        mapQuery: (query) => `https://duckduckgo.com/ac/?q=${encodeURIComponent(query)}`,
-        parseResponse: (res) => JSON.parse(res).map(ac => ac.phrase)
-    },
-    bing: {
-        mapQuery: (query) => `https://www.bing.com/AS/Suggestions?qry=${encodeURIComponent(query)}&cvid=%01&bareServer=`,
-        parseResponse: (res) => ([...res.matchAll(/<span class="sa_tm_text">(.*?)<\/span>/g)]).map(phrase => phrase[1].replace(/<strong>|<\/strong>/g, ''))
-    },
-    brave: {
-        mapQuery: (query) => `https://search.brave.com/api/suggest?q=${encodeURIComponent(query)}`,
-        parseResponse: (res) => JSON.parse(res)[1]
-    },
-    startpage: {
-        mapQuery: (query) => `https://www.startpage.com/suggestions?q=${encodeURIComponent(query)}&segment=omnibox`,
-        parseResponse: (res) => JSON.parse(res).suggestions.map(ac => ac.text)
-    },
-    ecosia: {
-        mapQuery: (query) => `https://ac.ecosia.org/?q=${encodeURIComponent(query)}`,
-        parseResponse: (res) => JSON.parse(res).suggestions
-    }
-};
 
-function ah() {
-    app.main.target.style.display = 'none';
-    app.header.target.style.display = 'none';
-
-    const frame = document.querySelector('.access-frame');
-    frame.src = './load.html#aHR0cHM6Ly9hbGllbmh1Yi54eXovP3V0bV9zb3VyY2U9aW5jb2dfZGVwbG95JnV0bV9tZWRpdW09YW1ldGh5c3RuZXR3b3Jr';
-
-    frame.style.display = 'block';
-    document.querySelector('.access-panel').style.removeProperty('display');
-}
 
 function access(app) {
+    app.closeNav();
     if (document.querySelector('header').hasAttribute('data-init')) {
         document.querySelector('header').removeAttribute('data-init')
     };
@@ -73,87 +17,122 @@ function access(app) {
     app.search.back.style.display = 'none';
     app.search.logo.style.display = 'inline';
     app.search.logo.style.marginLeft = '0';
-    app.search.submit.style.display = 'inline';
-    app.search.input.style.removeProperty('display');
-    app.search.input.placeholder = 'Search the web';
+    app.search.pd_round.submit.style.display = 'inline';
+    app.search.pd_round.input.style.removeProperty('display');
+    app.search.pd_round.input.placeholder = 'Search the web';
+    app.search.pd_round.style.display = 'flex';
+    app.search.pd_round.sep.style.display = 'flex';
     app.header.target.setAttribute('data-page', '');
     app.nav.target.style.removeProperty('display');
     document.querySelector('#open-nav').setAttribute('data-open', '');
-    app.search.input.focus();
-
+    app.search.pd_round.input.focus();
 
     
-    app.nav.community = app.createLink('#community', 'Community');
-    app.nav.support = app.createLink('#support', 'Support');
-    app.nav.apps = app.createLink('#apps', 'Apps');
-    app.nav.games = app.createLink('#gs', 'Games');
-    app.nav.settings = app.createLink('#settings', '<i class="fas fa-sliders-h secondary"></i>', {
+    app.nav.community = app.createLink('#discord', '</div><i class=""></i>');
+    app.nav.support = app.createLink('#support', '<i class="fa-solid fa-question secondary"></i>');
+    app.nav.apps = app.createLink('#apps', '<i class="fa-brands fa-app-store-ios secondary"></i>');
+    app.nav.games = app.createLink('#gs', '<i class="fa-solid fa-gamepad secondary"></i>');
+    app.nav.filter_bypass = app.createLink('#fb', '<i class="fa-solid fa-unlock secondary"></i>');
+    app.nav.settings = app.createLink('#settings', '<i class="fa-solid fa-cog secondary"></i>', {
         id: 'apps'
     })
-    app.main.tip = app.createElement('div', (localStorage.getItem('incog||disabletips') !== 'none' ? tips[Math.floor(Math.random()*tips.length)] : ''), { class: 'tip' });
 
-	async function searchSuggestions(event) {
-		app.main.suggestions.innerHTML = '';
-		if (!event.target.value) {
-			app.nav.target.style.removeProperty('display');
-			app.header.target.setAttribute('data-page', '');
-			app.main.tip.style.removeProperty('display');
-			app.search.logo.style.display = 'inline';
-			return;
-		}
+    app.main.tip = app.createElement('div', tips[Math.floor(Math.random() * tips.length)], {
+        class: 'tip'
+    });
 
-		app.main.tip.style.display = 'none';
-		app.header.target.removeAttribute('data-page');
-		app.nav.target.style.display = 'none';
-		app.search.logo.style.display = 'none';
 
-		clearTimeout(app.timeout);
-		app.timeout = setTimeout(async () => {
-			const provider = searchProviders[localStorage.getItem('incog||suggestions') || 'ddg'];
-			const res = await app.bare.fetch(provider.mapQuery(event.target.value));
-			const text = await res.text();
-			const suggestions = provider.parseResponse(text);
+    app.main.suggestions = app.createElement('div', [], {
+        class: 'suggestions',
+        style: {
+            display: 'block'
+        }
+    });
 
-			suggestions.forEach(element => {
-				app.main.suggestions.append(app.createElement('div', element, { class: 'suggestion',
-						events: {
-							click() {
-								app.search.input.value = element;
-								const frame = document.querySelector('iframe');
-								document.querySelector('main').style.display = 'none';
-								document.querySelector('header').style.display = 'none';
-								frame.style.display = 'block';
-								frame.src = './load.html#' + encodeURIComponent(btoa(element));
-								document.querySelector('.access-panel').style.removeProperty('display');
-							}
-						}
-					}))
+    app.search.pd_round.input.setAttribute(
+        'oninput',
+        '(' + (async function () {
+            app.main.suggestions.innerHTML = '';
+            if (!event.target.value) {
+                app.nav.target.style.removeProperty('display');
+                app.header.target.setAttribute('data-page', '');
+                app.main.tip.style.removeProperty('display');
+                app.search.logo.style.display = 'inline';
+                return;
+            }
+            app.main.tip.style.display = 'none';
+            app.header.target.removeAttribute('data-page');
+            app.nav.target.style.display = 'none';
+            app.search.logo.style.display = 'none';
 
-				});
-		}, 400);
-	}
+            clearTimeout(app.timeout);
+            app.timeout = setTimeout(async () => {
+                var mode = localStorage.getItem('incog||suggestions') || 'ddg';
+                var path;
+                var host;
+                var prefix;
+                var array;
+                if(mode == 'none') {} else {
+                    switch(mode) {
+                        case 'ddg':
+                            host = 'duckduckgo.com'
+                            path = '/ac/?q='
+                            prefix = 'phrase'
+                            array = false
+                            break;
+                        case 'brave':
+                            host = 'search.brave.com'
+                            path = '/api/suggest?q='
+                            array = true
+                            break;
+                    }
+                    const res = await fetch(__uv$config.bare + 'v1/', {
+                        headers: {
+                            'x-bare-host': host,
+                            'x-bare-protocol': 'https:',
+                            'x-bare-path': path + encodeURIComponent(event.target.value),
+                            'x-bare-port': '443',
+                            'x-bare-headers': JSON.stringify({ Host: host }),
+                            'x-bare-forward-headers': '[]'
+                        }
+                    })
+                    const json = await res.json();
+                    var suggestions = [];
 
-	if(localStorage.getItem('incog||suggestions') !== 'none') {
-    	app.main.suggestions = app.createElement('div', [], {
-        	class: 'suggestions',
-        	style: {
-            	display: 'block'
-        	} 
-    	});
+                    if(array) { suggestions = json[1] } else {
+                        json.forEach(element => suggestions.push(element[prefix]));
+                    }
 
-		app.search.input.addEventListener('input', searchSuggestions);
-		app.once('exit', () => app.search.input.removeEventListener('input', searchSuggestions));
-	}
+                    suggestions.forEach(element => {
+                        app.main.suggestions.append(app.createElement('div', element, { class: 'suggestion',
+                                events: {
+                                    click() {
+                                        app.search.pd_round.input.value = element;
+                                        const frame = document.querySelector('iframe');
+                                        document.querySelector('main').style.display = 'none';
+                                        document.querySelector('header').style.display = 'none';
+                                        frame.style.display = 'block';
+                                        frame.src = './load.html#' + encodeURIComponent(btoa(element));
+                                        document.querySelector('.access-panel').style.removeProperty('display');
+                                    }
+                                }
+                            }))
 
-    app.search.input.setAttribute('form', 'access-form');
-    app.search.submit.setAttribute('form', 'access-form');
+                    });
+            }
+            }, 400);
+
+        }).toString() + ')()'
+    );
+    app.search.pd_round.input.setAttribute('form', 'access-form');
+    app.search.pd_round.submit.setAttribute('form', 'access-form');
 
     const params = new URLSearchParams(window.location.search);
 
     if (params.has('link')) {
         app.main.target.style.display = 'none';
         app.header.target.style.display = 'none';
-        
+
         const frame = document.querySelector('.access-frame');
 
         frame.src = './load.html#' + encodeURIComponent(params.get('link'));
